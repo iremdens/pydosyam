1f280946792b412403451ba512f37d158cbfe0df
İrem Şimşek <bmirror59@gmail.com>
2025-06-18 01:11:20 +0300

    s

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 isim = input("Adınızı girinnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnndfghjnnnn:: ")
-print(f"Merhaba, {isim}! Python dünyassfgfsdfaaaaaaadfgdfafgfdffgına sdfghjjwcdbssndkxxxxxxxxxxxxxxxxmsdfghmögfddvbnsdddddddddhoş geldin ")
+print(f"Merhaba, {isim}! Python dünyassfgfsdfaaaaaaadfgdfafgfdffgına sdfghjjwcdbssndkxxxxxxxxxxxxxxxxmsdfghmögfddvbnsdfghjk ddddddddhoş geldin ")
+
