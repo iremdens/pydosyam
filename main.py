2c066e97edd9e2c491429d0aae0e18e254ee9348
İrem Şimşek <bmirror59@gmail.com>
2025-06-18 00:47:19 +0300

    hadi be

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 isim = input("Adınızı girin: ")
-print(f"Merhaba, {isim}! Python dünyassfgfsdfaaaaaaaafgfdfına ddddddddhoş geldin ")
+print(f"Merhaba, {isim}! Python dünyassfgfsdfaaaaaaaafgfdfına sdfghjjwssndkmnsdddddddddhoş geldin ")
